9f4a4f25fe3fc22ce05befe21f1e518b75cdaacd
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-07-09 14:46:20 +0200

    Add copy semantics to memory regions.

--- src/gb/memory.cc
+++ src/gb/memory.cc
@@ -1,7 +1,33 @@
 #include "memory.h"
+#include <algorithm>
+#include <utility>
 
 namespace gb {
 
 Memory::Memory(const std::uint16_t offset, const std::size_t size)
-    : offset{offset}, data{new std::uint8_t[size]} {}
+    : offset{offset}, size{size}, data{new std::uint8_t[size]} {}
+
+Memory::Memory(const Memory& other)
+    : offset{other.offset}, size{other.size}, data{new std::uint8_t[other.size]} {
+    std::copy(other.data.get(), other.data.get() + other.size, this->data.get());
+}
+
+Memory::Memory(Memory&& other) noexcept : Memory{} {
+    Swap(*this, other);
+}
+
+Memory& Memory::operator=(Memory other) {
+    Swap(*this, other);
+    return *this;
+}
+
+Memory::Memory() : offset{}, size{}, data{} {}
+
+void Swap(Memory& lhs, Memory& rhs) {
+    using std::swap;
+    swap(lhs.offset, rhs.offset);
+    swap(lhs.size, rhs.size);
+    swap(lhs.data, rhs.data);
+}
+
 }
--- src/gb/memory.h
+++ src/gb/memory.h
@@ -9,10 +9,18 @@ namespace gb {
 class Memory {
 public:
     Memory(std::uint16_t offset, std::size_t size);
+    Memory(const Memory& other);
+    Memory(Memory&& other) noexcept;
+    Memory& operator=(Memory other);
     std::uint8_t& operator[](std::uint16_t address);
     std::uint8_t operator[](std::uint16_t address) const;
+    std::uint16_t Offset() const;
+    std::size_t Size() const;
+    friend void Swap(Memory& lhs, Memory& rhs);
 private:
+    Memory();
     std::uint16_t offset;
+    std::size_t size;
     std::unique_ptr<std::uint8_t[]> data;
 };
 
@@ -23,5 +31,11 @@ inline std::uint8_t& Memory::operator[](const std::uint16_t address) {
 inline std::uint8_t Memory::operator[](const std::uint16_t address) const {
     return data[address - this->offset];
 }
+
+inline std::uint16_t Memory::Offset() const { return this->offset; }
+
+inline std::size_t Memory::Size() const { return this->size; }
+
+void Swap(Memory& lhs, Memory& rhs);
 
 }
