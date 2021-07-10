dbd86ca6bc42d3725e7d9c6d91a23bd046775269
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-07-10 20:25:54 +0200

    Make the instance class a moveable singleton.

--- src/media/instance.cc
+++ src/media/instance.cc
@@ -1,22 +1,46 @@
 #include "instance.h"
 #include <stdexcept>
+#include <utility>
 #include <SDL.h>
 #include <SDL_error.h>
 
 namespace sdl {
 
-Instance_::Instance_() {
-   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-      throw std::runtime_error{SDL_GetError()};
-   }
+Instance_::Instance_() : active{true} {
+    if (instanceExists) {
+        throw std::runtime_error{"Only one SDL instance is allowed at a time."};
+    }
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+       throw std::runtime_error{SDL_GetError()};
+    }
+    instanceExists = true;
+}
+
+Instance_::Instance_(Instance_&& other) noexcept : active{false} {
+    Swap(*this, other);
 }
 
 Instance_::~Instance_() {
-   SDL_Quit();
+    if (active) {
+        SDL_Quit();
+        instanceExists = false;
+    }
 }
 
 Instance Instance_::Create() {
     return Instance{new Instance_};
+}
+
+Instance_& Instance_::operator=(Instance_&& other) noexcept {
+    Swap(*this, other);
+    return *this;
+}
+
+bool Instance_::instanceExists{false};
+
+void Swap(Instance_& lhs, Instance_& rhs) noexcept {
+    using std::swap;
+    swap(lhs.active, rhs.active);
 }
 
 }
--- src/media/instance.h
+++ src/media/instance.h
@@ -8,12 +8,19 @@ using Instance = std::shared_ptr<const Instance_>;
 
 class Instance_ {
 public:
-    ~Instance_();
     Instance_(const Instance_&) = delete;
+    Instance_(Instance_&& other) noexcept;
+    ~Instance_();
     Instance_& operator=(const Instance_&) = delete;
+    Instance_& operator=(Instance_&& other) noexcept;
     static Instance Create();
 private:
     Instance_();
+    static bool instanceExists;
+    bool active;
+    friend void Swap(Instance_& lhs, Instance_& rhs) noexcept;
 };
+
+void Swap(Instance_& lhs, Instance_& rhs) noexcept;
 
 }
