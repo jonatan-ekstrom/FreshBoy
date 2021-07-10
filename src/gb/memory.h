0787881ae776dfd501ada78bbab331cfb766daa7
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-07-10 13:27:07 +0200

    Simplify Memory class by using a vector instead of unique_ptr.

--- src/gb/memory.h
+++ src/gb/memory.h
@@ -2,40 +2,32 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <memory>
+#include <vector>
 
 namespace gb {
 
 class Memory {
 public:
     Memory(std::uint16_t offset, std::size_t size);
-    Memory(const Memory& other);
-    Memory(Memory&& other) noexcept;
-    Memory& operator=(Memory other);
     std::uint8_t& operator[](std::uint16_t address);
     std::uint8_t operator[](std::uint16_t address) const;
     std::uint16_t Offset() const;
     std::size_t Size() const;
-    friend void Swap(Memory& lhs, Memory& rhs);
 private:
-    Memory();
     std::uint16_t offset;
-    std::size_t size;
-    std::unique_ptr<std::uint8_t[]> data;
+    std::vector<std::uint8_t> bytes;
 };
 
 inline std::uint8_t& Memory::operator[](const std::uint16_t address) {
-    return data[address - this->offset];
+    return this->bytes[address - this->offset];
 }
 
 inline std::uint8_t Memory::operator[](const std::uint16_t address) const {
-    return data[address - this->offset];
+    return this->bytes[address - this->offset];
 }
 
 inline std::uint16_t Memory::Offset() const { return this->offset; }
 
-inline std::size_t Memory::Size() const { return this->size; }
-
-void Swap(Memory& lhs, Memory& rhs);
+inline std::size_t Memory::Size() const { return this->bytes.size(); }
 
 }
