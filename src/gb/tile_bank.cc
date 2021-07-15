4694d7724fafff78cddf2e0b61937f7571e78aca
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-07-15 20:48:47 +0200

    Implement class representing the VRAM tile bank.

---
+++ src/gb/tile_bank.cc
@@ -0,0 +1,82 @@
+#include "tile_bank.h"
+#include <stdexcept>
+#include <tuple>
+
+namespace {
+
+constexpr auto NumTiles{384u};
+constexpr auto TileSize{16u};
+constexpr auto StartAddress{0x8000u};
+constexpr auto EndAddress{0x97FFu};
+
+unsigned int GetBit(const std::uint8_t byte, const unsigned int bit) {
+    const auto mask = 1u << bit;
+    return (byte & mask) != 0 ? 1 : 0;
+}
+
+bool ValidAddress(const std::uint16_t address) {
+    return address >= StartAddress && address <= EndAddress;
+}
+
+auto GetTileAndIndex(const std::uint16_t address) {
+    const auto adjusted{address - StartAddress};
+    const auto tile{adjusted / TileSize};
+    const auto index{adjusted % TileSize};
+    return std::make_tuple(tile, index);
+}
+
+}
+
+namespace gb {
+
+Tile::Tile() : data{} {}
+
+std::uint8_t Tile::Read(const unsigned int index) const {
+    return this->data[index];
+}
+
+void Tile::Write(const unsigned int index, const std::uint8_t byte) {
+    this->data[index] = byte;
+}
+
+ColorIndex Tile::Dot(const unsigned int row, const unsigned int col) const {
+    const auto lowByte{this->data[row * 2]};
+    const auto highByte{this->data[row * 2 + 1]};
+    const auto highBit{GetBit(highByte, col)};
+    const auto lowBit{GetBit(lowByte, col)};
+    const auto color{highBit << 1 | lowBit};
+    return static_cast<ColorIndex>(color);
+}
+
+TileBank::TileBank() : tiles(NumTiles) {}
+
+std::uint8_t TileBank::Read(const std::uint16_t address) const {
+    if (!ValidAddress(address)) {
+        throw std::runtime_error{"TileBank - Invalid read access."};
+    }
+    unsigned int tile, index;
+    std::tie(tile, index) = GetTileAndIndex(address);
+    return this->tiles[tile].Read(index);
+}
+
+void TileBank::Write(const std::uint16_t address, const std::uint8_t byte) {
+    if (!ValidAddress(address)) {
+        throw std::runtime_error{"TileBank - Invalid write access."};
+    }
+    unsigned int tile, index;
+    std::tie(tile, index) = GetTileAndIndex(address);
+    this->tiles[tile].Write(index, byte);
+}
+
+Tile TileBank::GetTileLow(const std::uint8_t index) const {
+    return this->tiles[index];
+}
+
+Tile TileBank::GetTileHigh(const std::uint8_t index) const {
+    constexpr auto base{256};
+    const auto sign{static_cast<std::int8_t>(index)};
+    const auto offset{static_cast<unsigned int>(base + sign)};
+    return this->tiles[offset];
+}
+
+}
---
+++ src/gb/tile_bank.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <vector>
+
+namespace gb {
+
+enum ColorIndex : unsigned int {
+    Zero,
+    One,
+    Two,
+    Three
+};
+
+class Tile {
+public:
+    Tile();
+    std::uint8_t Read(unsigned int index) const;
+    void Write(unsigned int index, std::uint8_t byte);
+    ColorIndex Dot(unsigned int row, unsigned int col) const;
+private:
+    std::array<std::uint8_t, 16> data;
+};
+
+class TileBank {
+public:
+    TileBank();
+    std::uint8_t Read(std::uint16_t address) const;
+    void Write(std::uint16_t address, std::uint8_t byte);
+    Tile GetTileLow(std::uint8_t index) const;
+    Tile GetTileHigh(std::uint8_t index) const;
+private:
+    std::vector<Tile> tiles;
+};
+
+}
