1bb5335a18abfafe169ba78daf23fedcd4657e0f
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-07-28 22:29:02 +0200

    Make Palette shareable.

--- src/gb/palettes.cc
+++ src/gb/palettes.cc
@@ -3,20 +3,20 @@
 
 namespace gb {
 
-Palette::Palette(const std::uint8_t byte, const bool object)
+Palette Palette_::Create(const std::uint8_t init, const bool obj) {
+    return Palette{new Palette_{init, obj}};
+}
+
+Palette_::Palette_(const std::uint8_t byte, const bool object)
     : data{}, object{object} {
     Write(byte);
 }
 
-Shade Palette::Map(const ColorIndex index) const {
-    return this->map.at(index);
-}
-
-std::uint8_t Palette::Read() const {
+std::uint8_t Palette_::Read() const {
     return this->data;
 }
 
-void Palette::Write(const std::uint8_t byte) {
+void Palette_::Write(const std::uint8_t byte) {
     const auto shade0{(byte & 0x03)};
     const auto shade1{(byte & 0x0C) >> 2};
     const auto shade2{(byte & 0x30) >> 4};
@@ -32,19 +32,26 @@ void Palette::Write(const std::uint8_t byte) {
     this->data = byte;
 }
 
-Palettes::Palettes() : bg{0xFC, false}, obj0{0xFF, true}, obj1{0xFF, true} {}
+Shade Palette_::Map(const ColorIndex index) const {
+    return this->map.at(index);
+}
+
+Palettes::Palettes()
+    : bg{Palette_::Create(0xFC, false)},
+      obj0{Palette_::Create(0xFF, true)},
+      obj1{Palette_::Create(0xFF, true)} {}
 
 std::uint8_t Palettes::Read(const std::uint16_t address) const {
     if (address == 0xFF47) {
-        return this->bg.Read();
+        return this->bg->Read();
     }
 
     if (address == 0xFF48) {
-        return this->obj0.Read();
+        return this->obj0->Read();
     }
 
     if (address == 0xFF49) {
-        return this->obj1.Read();
+        return this->obj1->Read();
     }
 
     throw std::runtime_error{"Palettes - Invalid read address."};
@@ -52,32 +59,32 @@ std::uint8_t Palettes::Read(const std::uint16_t address) const {
 
 void Palettes::Write(const std::uint16_t address, const std::uint8_t byte) {
     if (address == 0xFF47) {
-        this->bg.Write(byte);
+        this->bg->Write(byte);
         return;
     }
 
     if (address == 0xFF48) {
-        this->obj0.Write(byte);
+        this->obj0->Write(byte);
         return;
     }
 
     if (address == 0xFF49) {
-        this->obj1.Write(byte);
+        this->obj1->Write(byte);
         return;
     }
 
     throw std::runtime_error{"Palettes - Invalid write address."};
 }
 
-const Palette& Palettes::Background() const {
+Palette Palettes::Background() const {
     return this->bg;
 }
 
-const Palette& Palettes::Object0() const {
+Palette Palettes::Object0() const {
     return this->obj0;
 }
 
-const Palette& Palettes::Object1() const {
+Palette Palettes::Object1() const {
     return this->obj1;
 }
 
--- src/gb/palettes.h
+++ src/gb/palettes.h
@@ -1,18 +1,23 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 #include <unordered_map>
 #include "color.h"
 
 namespace gb {
 
-class Palette {
+class Palette_;
+using Palette = std::shared_ptr<Palette_>;
+
+class Palette_ {
 public:
-    Palette(std::uint8_t byte, bool object);
-    Shade Map(ColorIndex index) const;
+    static Palette Create(std::uint8_t init, bool obj);
     std::uint8_t Read() const;
     void Write(std::uint8_t byte);
+    Shade Map(ColorIndex index) const;
 private:
+    Palette_(std::uint8_t init, bool object);
     std::unordered_map<ColorIndex, Shade> map;
     std::uint8_t data;
     bool object;
@@ -23,9 +28,9 @@ public:
     Palettes();
     std::uint8_t Read(std::uint16_t address) const;
     void Write(std::uint16_t address, std::uint8_t byte);
-    const Palette& Background() const;
-    const Palette& Object0() const;
-    const Palette& Object1() const;
+    Palette Background() const;
+    Palette Object0() const;
+    Palette Object1() const;
 private:
     Palette bg;
     Palette obj0;
