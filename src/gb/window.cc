1131cd805e23c97ce7b2380b059e28e30de2572c
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-07-29 13:05:05 +0200

    Implement scanline rendering for the window.

--- src/gb/window.cc
+++ src/gb/window.cc
@@ -2,6 +2,14 @@
 #include <stdexcept>
 #include <utility>
 
+namespace {
+
+std::vector<gb::Shade> GetTransparent(const unsigned int count) {
+    return std::vector<gb::Shade>{count, gb::Shade::Transparent};
+}
+
+}
+
 namespace gb {
 
 Window::Window(TileBanks banks, TileMaps maps, Palette palette)
@@ -57,13 +65,57 @@ void Window::UseMap(const TileMap map) {
     this->activeMap = map;
 }
 
-std::vector<Shade> Window::RenderScanline(const unsigned int) const {
-    return std::vector<Shade>();
+std::vector<Shade> Window::RenderScanline(const unsigned int line) const {
+    constexpr auto displayHeight{144u};
+    if (line >= displayHeight) {
+        throw std::runtime_error{"Window - invalid scanline."};
+    }
+
+    constexpr auto displayWidth{160u};
+    if (!this->enabled) {
+        return GetTransparent(displayWidth);
+    }
+
+    const auto originY{this->wY};
+    if (line < originY) {
+        return GetTransparent(displayWidth);
+    }
+
+    const auto originX{this->wX - 7u};
+    if (originX >= displayWidth) {
+        return GetTransparent(displayWidth);
+    }
+
+    const auto displayY{line};
+    const auto mapY{displayY - originY};
+    std::vector<Shade> scanline{GetTransparent(displayWidth)};
+    for (auto displayX{originX}; displayX < displayWidth; ++displayX) {
+        const auto mapX{displayX - originX};
+        scanline[displayX] = this->palette->Map(GetDot(mapX, mapY));
+    }
+
+    return scanline;
 }
 
-ColorIndex Window::GetDot(const unsigned int,
-                          const unsigned int) const {
-    return One;
+ColorIndex Window::GetDot(const unsigned int mapX,
+                          const unsigned int mapY) const {
+    constexpr auto tileSize{8u};
+    constexpr auto tilesPerLine{32u};
+    const auto tileX{mapX / tileSize};
+    const auto dotX{mapX % tileSize};
+    const auto tileY{mapY / tileSize};
+    const auto dotY{mapY % tileSize};
+
+    const auto tileMap{this->activeMap == TileMap::Low ?
+                       this->maps->LowMap() :
+                       this->maps->HighMap()};
+    const auto mapIndex{tileY * tilesPerLine + tileX};
+    const auto tileIndex{tileMap[mapIndex]};
+
+    const auto tile{this->activeBank == TileBank::Low ?
+                    this->banks->GetTileLow(tileIndex) :
+                    this->banks->GetTileHigh(tileIndex)};
+    return tile.Dot(dotX, dotY);
 }
 
 }
