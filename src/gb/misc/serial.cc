8e34a3df9f300ab7b7d874567e3b46f19a1b9cf1
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-14 17:46:24 +0200

    Make Serial shareable.

--- src/gb/misc/serial.cc
+++ src/gb/misc/serial.cc
@@ -15,20 +15,24 @@ constexpr void ClearBit(std::uint8_t& reg, const unsigned int bit) {
 
 namespace gb {
 
-Serial::Serial(InterruptManager interrupts)
+Serial_::Serial_(InterruptManager&& interrupts)
     : interrupts{std::move(interrupts)},
       sb{0},
       sc{0},
       cycleCount{0},
       shifts{0} {}
 
-std::uint8_t Serial::Read(const std::uint16_t address) const {
+Serial Serial_::Create(InterruptManager interrupts) {
+    return Serial{new Serial_{std::move(interrupts)}};
+}
+
+std::uint8_t Serial_::Read(const std::uint16_t address) const {
     if (address == SbAddress) return this->sb;
     if (address == ScAddress) return this->sc;
     throw std::runtime_error{"Serial - invalid read address."};
 }
 
-void Serial::Write(const std::uint16_t address, const std::uint8_t byte) {
+void Serial_::Write(const std::uint16_t address, const std::uint8_t byte) {
     if (address == SbAddress) {
         this->sb = byte;
         return;
@@ -42,7 +46,7 @@ void Serial::Write(const std::uint16_t address, const std::uint8_t byte) {
     throw std::runtime_error{"Serial - invalid write address."};
 }
 
-void Serial::Tick(const unsigned int cycles) {
+void Serial_::Tick(const unsigned int cycles) {
     constexpr auto divider{512};
     this->cycleCount += cycles;
     const auto numShifts{this->cycleCount / divider};
@@ -52,29 +56,29 @@ void Serial::Tick(const unsigned int cycles) {
     }
 }
 
-bool Serial::TransferInProgress() const {
+bool Serial_::TransferInProgress() const {
     return this->sc == 0x81;
 }
 
-void Serial::ScWrite(const std::uint8_t byte) {
+void Serial_::ScWrite(const std::uint8_t byte) {
     this->sc = byte & 0x81;
     if (this->sc == 0x81) {
         StartTransfer();
     }
 }
 
-void Serial::StartTransfer() {
+void Serial_::StartTransfer() {
     this->shifts = 0;
 }
 
-void Serial::Shift() {
+void Serial_::Shift() {
     this->sb = static_cast<uint8_t>(this->sb << 1);
     if (this->shifts++ == 8) {
         TransferDone();
     }
 }
 
-void Serial::TransferDone() {
+void Serial_::TransferDone() {
     ClearBit(this->sc, 7);
     this->shifts = 0;
     this->interrupts->RequestInterrupt(Interrupt::Serial);
--- src/gb/misc/serial.h
+++ src/gb/misc/serial.h
@@ -1,16 +1,21 @@
 #pragma once
 #include <cstdint>
+#include <memory>
 #include "interrupt.h"
 
 namespace gb {
 
-class Serial {
+class Serial_;
+using Serial = std::shared_ptr<Serial_>;
+
+class Serial_ {
 public:
-    explicit Serial(InterruptManager interrupts);
+    static Serial Create(InterruptManager interrupts);
     std::uint8_t Read(std::uint16_t address) const;
     void Write(std::uint16_t address, std::uint8_t byte);
     void Tick(unsigned int cycles);
 private:
+    explicit Serial_(InterruptManager&& interrupts);
     bool TransferInProgress() const;
     void ScWrite(std::uint8_t byte);
     void StartTransfer();
