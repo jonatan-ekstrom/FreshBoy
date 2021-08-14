635ce5d08bbd89f986b95f1e466d7ec2ffa2c9c8
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-14 14:23:24 +0200

    Implement the timer component.

--- src/gb/misc/timer.cc
+++ src/gb/misc/timer.cc
@@ -1,1 +1,209 @@
 #include "timer.h"
+#include <stdexcept>
+#include <utility>
+
+namespace {
+
+constexpr auto DivAddress{0xFF04};
+constexpr auto TimaAddress{0xFF05};
+constexpr auto TmaAddress{0xFF06};
+constexpr auto TacAddress{0xFF07};
+
+template<typename T>
+constexpr bool BitSet(const T reg, const unsigned int bit) {
+    return (reg & (1 << bit)) != 0;
+}
+
+}
+
+namespace gb {
+
+Timer::Timer(InterruptManager interrupts)
+    : interrupts{std::move(interrupts)},
+      ticks{0},
+      tima{0},
+      tma{0},
+      tac{0},
+      state{TimerState::Normal},
+      stateCounter{0} {}
+
+std::uint8_t Timer::Read(const std::uint16_t address) const {
+    if (address == DivAddress) {
+        return Div();
+    }
+
+    if (address == TimaAddress) {
+        return this->tima;
+    }
+
+    if (address == TmaAddress) {
+        return this->tma;
+    }
+
+    if (address == TacAddress) {
+        return this->tac;
+    }
+
+    throw std::runtime_error{"Timer - invalid read address."};
+}
+
+void Timer::Write(const std::uint16_t address, const std::uint8_t byte) {
+    if (address == DivAddress) {
+        Reset();
+        return;
+    }
+
+    if (address == TimaAddress) {
+        TimaWrite(byte);
+        return;
+    }
+
+    if (address == TmaAddress) {
+        TmaWrite(byte);
+        return;
+    }
+
+    if (address == TacAddress) {
+        TacWrite(byte);
+        return;
+    }
+
+    throw std::runtime_error{"Timer - invalid write address."};
+}
+
+void Timer::Tick(const unsigned int cycles) {
+    for (auto i{0u}; i < cycles; ++i) {
+        Tick();
+    }
+}
+
+bool Timer::Output() const {
+    const auto enabled{BitSet(this->tac, 2)};
+    if (!enabled) return false;
+
+    const auto freq{this->tac & 0x03};
+    unsigned int bit;
+    switch (freq) {
+        case 0:
+            bit = 9;
+            break;
+        case 1:
+            bit = 3;
+            break;
+        case 2:
+            bit = 5;
+            break;
+        case 3:
+            bit = 7;
+            break;
+        default:
+            throw std::runtime_error{"Timer - invalid frequency."};
+    }
+
+    return BitSet(this->ticks, bit);
+}
+
+std::uint8_t Timer::Div() const {
+    return static_cast<std::uint8_t>((this->ticks >> 8) & 0xFF);
+}
+
+void Timer::FireInterrupt() const {
+    this->interrupts->RequestInterrupt(Interrupt::Timer);
+}
+
+void Timer::Reset() {
+    const auto prev{Output()};
+    this->ticks = 0;
+    if (prev) {
+        TimaTick();
+    }
+}
+
+void Timer::TimaWrite(const std::uint8_t byte) {
+    switch (this->state) {
+        case TimerState::Normal:
+            this->tima = byte;
+            break;
+        case TimerState::Overflow:
+            this->tima = byte;
+            GotoState(TimerState::Normal);
+            break;
+        case TimerState::Load:
+            LoadTima();
+            break;
+        default:
+            throw std::runtime_error{"Timer - invalid state."};
+    }
+}
+
+void Timer::TmaWrite(const std::uint8_t byte) {
+    this->tma = byte;
+    if (this->state == TimerState::Load) {
+        LoadTima();
+    }
+}
+
+void Timer::TacWrite(const std::uint8_t byte) {
+    const auto prev{Output()};
+    this->tac = byte & 0x07;
+    const auto curr{Output()};
+    if (prev && !curr) {
+        TimaTick();
+    }
+}
+
+void Timer::Tick() {
+    RunStateMachine();
+    if (IncrementCounter()) {
+        TimaTick();
+    }
+}
+
+void Timer::TimaTick() {
+    ++this->tima;
+    if (this->tima == 0) {
+        GotoState(TimerState::Overflow);
+    }
+}
+
+void Timer::RunStateMachine() {
+    constexpr auto cycleLength{4};
+    ++this->stateCounter;
+    switch (this->state) {
+        case TimerState::Normal:
+            GotoState(TimerState::Normal);
+            break;
+        case TimerState::Overflow:
+            if (this->stateCounter == cycleLength) {
+                GotoState(TimerState::Load);
+                LoadTima();
+                FireInterrupt();
+            }
+            break;
+        case TimerState::Load:
+            if (this->stateCounter == cycleLength) {
+                GotoState(TimerState::Normal);
+            }
+            break;
+        default:
+            throw std::runtime_error{"Timer - invalid state."};
+    }
+}
+
+bool Timer::IncrementCounter() {
+    const auto prev{Output()};
+    ++this->ticks;
+    const auto curr{Output()};
+    return (prev && !curr);
+}
+
+void Timer::LoadTima() {
+    this->tima = this->tma;
+}
+
+void Timer::GotoState(const TimerState newState) {
+    this->state = newState;
+    this->stateCounter = 0;
+}
+
+}
--- src/gb/misc/timer.h
+++ src/gb/misc/timer.h
@@ -1,1 +1,42 @@
 #pragma once
+#include <cstdint>
+#include "interrupt.h"
+
+namespace gb {
+
+enum class TimerState {
+    Normal,
+    Overflow,
+    Load
+};
+
+class Timer {
+public:
+    explicit Timer(InterruptManager interrupts);
+    std::uint8_t Read(std::uint16_t address) const;
+    void Write(std::uint16_t address, std::uint8_t byte);
+    void Tick(unsigned int cycles);
+private:
+    bool Output() const;
+    std::uint8_t Div() const;
+    void FireInterrupt() const;
+    void Reset();
+    void TimaWrite(std::uint8_t byte);
+    void TmaWrite(std::uint8_t byte);
+    void TacWrite(std::uint8_t byte);
+    void Tick();
+    void TimaTick();
+    void RunStateMachine();
+    bool IncrementCounter();
+    void LoadTima();
+    void GotoState(TimerState newState);
+    InterruptManager interrupts;
+    std::uint16_t ticks;
+    std::uint8_t tima;
+    std::uint8_t tma;
+    std::uint8_t tac;
+    TimerState state;
+    unsigned int stateCounter;
+};
+
+}
