337645b7ea623aef5059ecb10e938cf16b9cbecf
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-15 21:03:21 +0200

    Add Channel3 registers.

--- src/gb/apu/channel_3.cc
+++ src/gb/apu/channel_3.cc
@@ -1,1 +1,80 @@
 #include "channel_3.h"
+#include <stdexcept>
+
+namespace {
+
+constexpr auto Nr30Address{0xFF1A};
+constexpr auto Nr31Address{0xFF1B};
+constexpr auto Nr32Address{0xFF1C};
+constexpr auto Nr33Address{0xFF1D};
+constexpr auto Nr34Address{0xFF1E};
+constexpr auto WaveBase{0xFF30};
+constexpr auto WaveTop{0xFF3F};
+
+}
+
+namespace gb {
+
+Channel3::Channel3() : nr30{0}, nr31{0}, nr32{0}, nr33{0}, nr34{0}, wave{} {}
+
+u8 Channel3::Read(const u16 address) const {
+    if (address == Nr30Address) {
+        return this->nr30 & 0x80;
+    }
+
+    if (address == Nr31Address) {
+        return this->nr31;
+    }
+
+    if (address == Nr32Address) {
+        return this->nr32 & 0x60;
+    }
+
+    // Nr33 is write-only.
+
+    if (address == Nr34Address) {
+        return this->nr34 & 0x40;
+    }
+
+    if (address >= WaveBase && address <= WaveTop) {
+        return this->wave[address - WaveBase];
+    }
+
+    throw std::runtime_error{"Channel3 - invalid read address."};
+}
+
+void Channel3::Write(const u16 address, const u8 byte) {
+    if (address == Nr30Address) {
+        this->nr30 = byte & 0x80;
+        return;
+    }
+
+    if (address == Nr31Address) {
+        this->nr31 = byte;
+        return;
+    }
+
+    if (address == Nr32Address) {
+        this->nr32 = byte & 0x60;
+        return;
+    }
+
+    if (address == Nr33Address) {
+        this->nr33 = byte;
+        return;
+    }
+
+    if (address == Nr34Address) {
+        this->nr34 = byte & 0xC7;
+        return;
+    }
+
+    if (address >= WaveBase && address <= WaveTop) {
+        this->wave[address - WaveBase] = byte;
+        return;
+    }
+
+    throw std::runtime_error{"Channel3 - invalid write address."};
+}
+
+}
--- src/gb/apu/channel_3.h
+++ src/gb/apu/channel_3.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <array>
+#include "types.h"
+
+namespace gb {
+
+class Channel3 {
+public:
+    Channel3();
+    u8 Read(u16 address) const;
+    void Write(u16 address, u8 byte);
+private:
+    u8 nr30;
+    u8 nr31;
+    u8 nr32;
+    u8 nr33;
+    u8 nr34;
+    std::array<u8, 16> wave;
+};
+
+}
