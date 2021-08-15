991ddc57804ebc9ba0162b9284b7c7832048ee2e
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-15 20:39:44 +0200

    Add Channel1 registers.

--- src/gb/apu/channel_1.cc
+++ src/gb/apu/channel_1.cc
@@ -1,1 +1,69 @@
 #include "channel_1.h"
+#include <stdexcept>
+
+namespace {
+
+constexpr auto Nr10Address{0xFF10};
+constexpr auto Nr11Address{0xFF11};
+constexpr auto Nr12Address{0xFF12};
+constexpr auto Nr13Address{0xFF13};
+constexpr auto Nr14Address{0xFF14};
+
+}
+
+namespace gb {
+
+Channel1::Channel1() : nr10{0}, nr11{0}, nr12{0}, nr13{0}, nr14{0} {}
+
+u8 Channel1::Read(const u16 address) const {
+    if (address == Nr10Address) {
+        return this->nr10 & 0x7F;
+    }
+
+    if (address == Nr11Address) {
+        return this->nr11 & 0xC0;
+    }
+
+    if (address == Nr12Address) {
+        return this->nr12;
+    }
+
+    // Nr13 is write-only.
+
+    if (address == Nr14Address) {
+        return this->nr14 & 0x40;
+    }
+
+    throw std::runtime_error{"Channel1 - invalid read address."};
+}
+
+void Channel1::Write(const u16 address, const u8 byte) {
+    if (address == Nr10Address) {
+        this->nr10 = byte & 0x7F;
+        return;
+    }
+
+    if (address == Nr11Address) {
+        this->nr11 = byte;
+        return;
+    }
+
+    if (address == Nr12Address) {
+        this->nr12 = byte;
+        return;
+    }
+
+    if (address == Nr13Address) {
+        this->nr13 = byte;
+        return;
+    }
+
+    if (address == Nr14Address) {
+        this->nr14 = byte & 0xC7;
+        return;
+    }
+
+    throw std::runtime_error{"Channel1 - invalid write address."};
+}
+
+}
--- src/gb/apu/channel_1.h
+++ src/gb/apu/channel_1.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "types.h"
+
+namespace gb {
+
+class Channel1 {
+public:
+    Channel1();
+    u8 Read(u16 address) const;
+    void Write(u16 address, u8 byte);
+private:
+    u8 nr10;
+    u8 nr11;
+    u8 nr12;
+    u8 nr13;
+    u8 nr14;
+};
+
+}
