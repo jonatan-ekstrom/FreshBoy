f7b4cdca47f7b78a7f0836832fdc45718f631363
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-15 20:53:26 +0200

    Add Channel2 registers.

--- src/gb/apu/channel_2.cc
+++ src/gb/apu/channel_2.cc
@@ -1,1 +1,59 @@
 #include "channel_2.h"
+#include <stdexcept>
+
+namespace {
+
+constexpr auto Nr21Address{0xFF16};
+constexpr auto Nr22Address{0xFF17};
+constexpr auto Nr23Address{0xFF18};
+constexpr auto Nr24Address{0xFF19};
+
+}
+
+namespace gb {
+
+Channel2::Channel2() : nr21{0}, nr22{0}, nr23{0}, nr24{0} {}
+
+u8 Channel2::Read(const u16 address) const {
+    if (address == Nr21Address) {
+        return this->nr21 & 0xC0;
+    }
+
+    if (address == Nr22Address) {
+        return this->nr22;
+    }
+
+    // Nr23 is write-only.
+
+    if (address == Nr24Address) {
+        return this->nr24 & 0x40;
+    }
+
+    throw std::runtime_error{"Channel2 - invalid read address."};
+}
+
+void Channel2::Write(const u16 address, const u8 byte) {
+    if (address == Nr21Address) {
+        this->nr21 = byte;
+        return;
+    }
+
+    if (address == Nr22Address) {
+        this->nr22 = byte;
+        return;
+    }
+
+    if (address == Nr23Address) {
+        this->nr23 = byte;
+        return;
+    }
+
+    if (address == Nr24Address) {
+        this->nr24 = byte & 0xC7;
+        return;
+    }
+
+    throw std::runtime_error{"Channel2 - invalid write address."};
+}
+
+}
--- src/gb/apu/channel_2.h
+++ src/gb/apu/channel_2.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "types.h"
+
+namespace gb {
+
+class Channel2 {
+public:
+    Channel2();
+    u8 Read(u16 address) const;
+    void Write(u16 address, u8 byte);
+private:
+    u8 nr21;
+    u8 nr22;
+    u8 nr23;
+    u8 nr24;
+};
+
+}
