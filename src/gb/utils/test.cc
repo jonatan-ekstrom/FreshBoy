c83472dac250929f9db45a9683c2acec3ed75846
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-20 15:12:06 +0200

    Move type check to own file.

---
+++ src/gb/utils/test.cc
@@ -0,0 +1,19 @@
+#include "types.h"
+
+namespace {
+
+using namespace gb;
+
+constexpr uint BitCount() {
+    auto i{static_cast<uint>(-1)};
+    uint count{0};
+    while (i != 0) {
+        i >>= 1;
+        ++count;
+    }
+    return count;
+}
+
+static_assert(BitCount() >= 32, "16-bit ints not supported.");
+
+}
