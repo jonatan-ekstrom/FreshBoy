92d49097e588adb49ee22cba939507271b350fda
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-20 12:38:09 +0200

    Assert that int uses > 16 bits.

--- src/gb/utils/types.h
+++ src/gb/utils/types.h
@@ -9,4 +9,20 @@ using u16 = std::uint16_t;
 using u32 = std::uint32_t;
 using s8 = std::int8_t;
 
+namespace check {
+
+constexpr uint BitCount() {
+    uint i{static_cast<uint>(-1)};
+    uint count{0};
+    while (i != 0) {
+        i >>= 1;
+        ++count;
+    }
+    return count;
+}
+
+static_assert(BitCount() >= 32, "16-bit ints not supported.");
+
+}
+
 }
