922399d8c058abe967417f70ca38e415abbdce93
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-21 08:56:30 +0200

    Add subtraction operations.

--- src/gb/cpu/alu_ops.cc
+++ src/gb/cpu/alu_ops.cc
@@ -6,7 +6,7 @@ namespace gb {
 void Cpu_::Add(const u8 imm) {
     const auto lhs{this->a.v};
     const auto rhs{imm};
-    this->a.v += rhs;
+    this->a.v += imm;
     this->flags.UpdateZ(this->a.v == 0);
     this->flags.UpdateN(false);
     this->flags.UpdateH(bit::Carry(lhs, rhs, 3));
@@ -32,6 +32,37 @@ void Cpu_::AddWithCarry(const ByteReg reg) {
 
 void Cpu_::AddWithCarry(const RegPair rp) {
     AddWithCarry(this->mmu->Read(rp.Addr()));
+}
+
+void Cpu_::Sub(const u8 imm) {
+    const auto lhs{this->a.v};
+    const auto rhs{imm};
+    this->a.v -= imm;
+    this->flags.UpdateZ(this->a.v == 0);
+    this->flags.UpdateN(true);
+    this->flags.UpdateH(bit::Borrow(lhs, rhs, 4));
+    this->flags.UpdateC(bit::Borrow(lhs, rhs, 8));
+}
+
+void Cpu_::Sub(const ByteReg reg) {
+    Sub(reg.v);
+}
+
+void Cpu_::Sub(const RegPair rp) {
+    Sub(this->mmu->Read(rp.Addr()));
+}
+
+void Cpu_::SubWithBorrow(const u8 imm) {
+    const auto borrow{this->flags.C() ? 1 : 0};
+    Sub(static_cast<u8>(imm - borrow));
+}
+
+void Cpu_::SubWithBorrow(const ByteReg reg) {
+    SubWithBorrow(reg.v);
+}
+
+void Cpu_::SubWithBorrow(const RegPair rp) {
+    SubWithBorrow(this->mmu->Read(rp.Addr()));
 }
 
 }
--- src/gb/utils/bits.h
+++ src/gb/utils/bits.h
@@ -65,16 +65,16 @@ constexpr u8 Low(const u16 reg) {
 }
 
 template<typename T>
-constexpr bool Carry(const T lhs, const T rhs, const uint bit) {
-    const auto mask{(1u << (bit + 1)) - 1};
+constexpr bool Carry(const T lhs, const T rhs, const uint from) {
+    const auto mask{(1u << (from + 1)) - 1};
     const auto l{static_cast<uint>(lhs)};
     const auto r{static_cast<uint>(rhs)};
     return ((l & mask) + (r & mask) > mask);
 }
 
 template<typename T>
-constexpr bool Borrow(const T lhs, const T rhs, const uint bit) {
-    const auto mask{(1 << bit) - 1};
+constexpr bool Borrow(const T lhs, const T rhs, const uint from) {
+    const auto mask{(1 << from) - 1};
     const auto l{static_cast<int>(lhs)};
     const auto r{static_cast<int>(rhs)};
     return ((l & mask) - (r & mask) < 0);
