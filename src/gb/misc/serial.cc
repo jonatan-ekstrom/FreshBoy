efb96b0f7ba9ec9b4a200c52a6cd4e3c37d5da15
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-28 11:50:11 +0200

    Fix off-by-one error in the serial component.

--- src/gb/misc/serial.cc
+++ src/gb/misc/serial.cc
@@ -71,7 +71,7 @@ void Serial_::StartTransfer() {
 
 void Serial_::Shift() {
     this->sb = static_cast<u8>((this->sb << 1) | 1);
-    if (this->shifts++ == 8) {
+    if (++this->shifts == 8) {
         TransferDone();
     }
 }
