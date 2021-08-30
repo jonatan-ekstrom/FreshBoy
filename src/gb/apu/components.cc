494b3e592d607a60e74f77f0c220abf4f7432d16
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-08-30 13:00:32 +0200

    Add Mixer component.

--- src/gb/apu/components.cc
+++ src/gb/apu/components.cc
@@ -1,4 +1,5 @@
 #include "components.h"
+#include "bits.h"
 
 namespace gb {
 
@@ -12,6 +13,38 @@ double Dac::Map(const u8 sample) {
     if (!this->enabled) return 0;
     if (sample >= 15) return 1;
     return -1 + 2 * (sample / 15.0);
+}
+
+Mixer::Mixer() : mask{0} {}
+
+u8 Mixer::Read() const {
+    return mask;
+}
+
+void Mixer::Write(const u8 byte) {
+    this->mask = byte;
+}
+
+std::tuple<double, double> Mixer::Mix(const double ch1, const double ch2,
+                                      const double ch3, const double ch4) const {
+    double left{0};
+    double right{0};
+
+    // Mix
+    if (bit::IsSet(this->mask, 7)) left += ch4;
+    if (bit::IsSet(this->mask, 6)) left += ch3;
+    if (bit::IsSet(this->mask, 5)) left += ch2;
+    if (bit::IsSet(this->mask, 4)) left += ch1;
+    if (bit::IsSet(this->mask, 3)) right += ch4;
+    if (bit::IsSet(this->mask, 2)) right += ch3;
+    if (bit::IsSet(this->mask, 1)) right += ch2;
+    if (bit::IsSet(this->mask, 0)) right += ch1;
+
+    // Normalize
+    left /= 4;
+    right /= 4;
+
+    return {left, right};
 }
 
 }
--- src/gb/apu/components.h
+++ src/gb/apu/components.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <tuple>
 #include "types.h"
 
 namespace gb {
@@ -10,6 +11,17 @@ public:
     double Map(u8 sample);
 private:
     bool enabled;
+};
+
+class Mixer {
+public:
+    Mixer();
+    u8 Read() const;
+    void Write(u8 byte);
+    std::tuple<double, double> Mix(double ch1, double ch2,
+                                   double ch3, double ch4) const;
+private:
+    u8 mask;
 };
 
 }
