ed95a487ccf239223032960d8835a919529d2eec
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-10-12 21:25:18 +0200

    Refactor SDL renderer wrapper.

--- src/media/renderer.cc
+++ src/media/renderer.cc
@@ -1,21 +1,47 @@
 #include "renderer.h"
-#include <stdexcept>
 #include <utility>
+#include <SDL_error.h>
 #include <SDL_render.h>
 #include "texture.h"
 
 namespace sdl {
 
-Renderer_::Renderer_(Instance instance, Window window)
+Renderer Renderer_::Create(Instance instance, Window window) {
+    return Renderer{new Renderer_{std::move(instance), std::move(window)}};
+}
+
+void Renderer_::Clear() {
+    if (SDL_RenderClear(this->handle) != 0) {
+        throw std::runtime_error{SDL_GetError()};
+    }
+}
+
+void Renderer_::Copy(Texture& texture) {
+    if (SDL_RenderCopy(this->handle, texture.Handle(), nullptr, nullptr) != 0) {
+        throw std::runtime_error{SDL_GetError()};
+    }
+}
+
+void Renderer_::Present() {
+    SDL_RenderPresent(this->handle);
+}
+
+Renderer_::Renderer_(Instance&& instance, Window&& window)
     : instance{std::move(instance)}, window{std::move(window)} {
     const auto flags{SDL_RENDERER_ACCELERATED};
     this->handle = SDL_CreateRenderer(this->window->Handle(), -1, flags);
     if (this->handle == nullptr) {
-        throw std::runtime_error{"Failed to create SDL renderer."};
+        throw std::runtime_error{SDL_GetError()};
     }
     const auto rgb{0};
     const auto alpha{255};
-    SDL_SetRenderDrawColor(this->handle, rgb, rgb, rgb, alpha);
+    if (SDL_SetRenderDrawColor(this->handle, rgb, rgb, rgb, alpha) != 0) {
+        throw std::runtime_error{SDL_GetError()};
+    }
+}
+
+SDL_Renderer* Renderer_::Handle() {
+    return this->handle;
 }
 
 Renderer_::~Renderer_() {
@@ -31,28 +57,6 @@ Renderer_::Renderer_(Renderer_&& other) noexcept : handle{nullptr}  {
 Renderer_& Renderer_::operator=(Renderer_&& other) noexcept {
     Swap(*this, other);
     return *this;
-}
-
-Renderer Renderer_::Create(Instance instance, Window window) {
-    return Renderer{new Renderer_{std::move(instance), std::move(window)}};
-}
-
-void Renderer_::Clear() {
-    SDL_RenderClear(this->handle);
-}
-
-void Renderer_::Copy(Texture& texture) {
-    if (SDL_RenderCopy(this->handle, texture.Handle(), nullptr, nullptr) != 0) {
-        throw std::runtime_error{SDL_GetError()};
-    }
-}
-
-void Renderer_::Present() {
-    SDL_RenderPresent(this->handle);
-}
-
-SDL_Renderer* Renderer_::Handle() {
-    return this->handle;
 }
 
 void Swap(Renderer_& lhs, Renderer_& rhs) noexcept {
--- src/media/renderer.h
+++ src/media/renderer.h
@@ -12,24 +12,41 @@ class Texture;
 class Renderer_;
 using Renderer = std::shared_ptr<Renderer_>;
 
+/*
+ * Class wrapping an SDL renderer. Handles allocation/deallocation of associated
+ * resources. Provides access to the native SDL_Renderer handle.
+ */
 class Renderer_ {
 public:
+    /*
+     * Static constructor, creates a new renderer targeting the provided window.
+     * Enables hardware acceleration and sets a black draw color.
+     */
+    static Renderer Create(Instance instance, Window window);
+
+    /* Clears the surface managed by the renderer (i.e. draws black pixels). */
+    void Clear();
+
+    /* Copies the contents of the provided texture to the renderer's backbuffer. */
+    void Copy(Texture& texture);
+
+    /* Presents the contents of the backbuffer to the renderer's drawing surface. */
+    void Present();
+
+    /* Returns a pointer to the wrapped SDL_Renderer. */
+    SDL_Renderer* Handle();
+
     ~Renderer_();
     Renderer_(const Renderer_&) = delete;
     Renderer_(Renderer_&& other) noexcept;
     Renderer_& operator=(const Renderer_&) = delete;
     Renderer_& operator=(Renderer_&& other) noexcept;
-    static Renderer Create(Instance instance, Window window);
-    void Clear();
-    void Copy(Texture& texture);
-    void Present();
-    SDL_Renderer* Handle();
 private:
-    Renderer_(Instance instance, Window window);
+    Renderer_(Instance&& instance, Window&& window);
+    friend void Swap(Renderer_& lhs, Renderer_& rhs) noexcept;
     Instance instance;
     Window window;
     SDL_Renderer* handle;
-    friend void Swap(Renderer_& lhs, Renderer_& rhs) noexcept;
 };
 
 void Swap(Renderer_& lhs, Renderer_& rhs) noexcept;
