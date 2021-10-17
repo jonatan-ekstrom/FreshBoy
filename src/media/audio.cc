0f344ffe51a4243526bfb49d9e6b3220950af4c7
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-10-17 08:15:42 +0200

    Refactor SDL audio API wrapper.

--- src/media/audio.cc
+++ src/media/audio.cc
@@ -8,8 +8,11 @@ namespace sdl {
 
 Audio::Audio(Instance instance)
     : instance{std::move(instance)} {
+    /*
+     * Setup the desired audio format.
+     * We're using 44.1 kHz stereo sound in 8-bit unsigned samples.
+     */
     SDL_AudioSpec desired{}, obtained{};
-
     desired.freq = 44100;
     desired.format = AUDIO_U8;
     desired.channels = 2;
@@ -21,15 +24,15 @@ Audio::Audio(Instance instance)
     }
 }
 
-Audio::~Audio() {
-    SDL_CloseAudioDevice(this->id);
-}
-
 void Audio::Queue(const Samples& left, const Samples& right) {
     if (left.size() != right.size()) {
         throw std::runtime_error{"Audio - left/right channel size mismatch."};
     }
 
+    /*
+     * Left and right channel audio samples are interleaved.
+     * LRLRLRLRLRLRLR ...
+     */
     const auto size{static_cast<unsigned int>(left.size())};
     Samples samples(2 * size);
     for (auto i{0u}; i < size; ++i) {
@@ -46,8 +49,25 @@ void Audio::Play() {
     SDL_PauseAudioDevice(this->id, 0);
 }
 
-void Audio::Pause() {
-    SDL_PauseAudioDevice(this->id, 1);
+Audio::~Audio() {
+    if (this->id != 0) {
+        SDL_CloseAudioDevice(this->id);
+    }
+}
+
+Audio::Audio(Audio&& other) noexcept : id{0} {
+    Swap(*this, other);
+}
+
+Audio& Audio::operator=(Audio&& other) noexcept {
+    Swap(*this, other);
+    return *this;
+}
+
+void Swap(Audio& lhs, Audio& rhs) noexcept {
+    using std::swap;
+    swap(lhs.instance, rhs.instance);
+    swap(lhs.id, rhs.id);
 }
 
 }
--- src/media/audio.h
+++ src/media/audio.h
@@ -5,17 +5,35 @@
 
 namespace sdl {
 
+/* High level API wrapper class for the SDL audio subsystem. */
 class Audio {
 public:
+    /* A collection of samples to play. Unsigned 8-bit samples are used. */
     using Samples = std::vector<std::uint8_t>;
+
+    /* Initializes the audio system and acquires necessary system resources. */
     explicit Audio(Instance instance);
-    ~Audio();
+
+    /*
+     * Adds additional sound data to the playback queue.
+     * The left and right sample collections must be of equal length.
+     */
     void Queue(const Samples& left, const Samples& right);
+
+    /* Begins playback. */
     void Play();
-    void Pause();
+
+    ~Audio();
+    Audio(const Audio&) = delete;
+    Audio(Audio&& other) noexcept;
+    Audio& operator=(const Audio&) = delete;
+    Audio& operator=(Audio&& other) noexcept;
 private:
+    friend void Swap(Audio& lhs, Audio& rhs) noexcept;
     Instance instance;
     std::uint32_t id;
 };
+
+void Swap(Audio& lhs, Audio& rhs) noexcept;
 
 }
