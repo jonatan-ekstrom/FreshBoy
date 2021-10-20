50c0bbad82a3416db68c31882053a932762074d4
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2021-10-20 21:38:25 +0200

    Reduce vertical spacing in SDL key class switch statements.

--- src/media/key.cc
+++ src/media/key.cc
@@ -8,59 +8,29 @@ Key::Key(const SDL_KeyboardEvent& event)
     : Code{}, Type{}, State{}, Repeat{event.repeat != 0} {
     // Parse keycode.
     switch (event.keysym.sym) {
-        case SDLK_UP:
-            Code = Code::Up;
-            break;
-        case SDLK_DOWN:
-            Code = Code::Down;
-            break;
-        case SDLK_LEFT:
-            Code = Code::Left;
-            break;
-        case SDLK_RIGHT:
-            Code = Code::Right;
-            break;
-        case SDLK_z:
-            Code = Code::Z;
-            break;
-        case SDLK_x:
-            Code = Code::X;
-            break;
-        case SDLK_BACKSPACE:
-            Code = Code::Backspace;
-            break;
-        case SDLK_RETURN:
-            Code = Code::Return;
-            break;
-        default:
-            Code = Code::Unknown;
-            break;
+        case SDLK_UP: Code = Code::Up; break;
+        case SDLK_DOWN: Code = Code::Down; break;
+        case SDLK_LEFT: Code = Code::Left; break;
+        case SDLK_RIGHT: Code = Code::Right; break;
+        case SDLK_z: Code = Code::Z; break;
+        case SDLK_x: Code = Code::X; break;
+        case SDLK_BACKSPACE: Code = Code::Backspace; break;
+        case SDLK_RETURN: Code = Code::Return; break;
+        default: Code = Code::Unknown; break;
     }
 
     // Parse event type.
     switch (event.type) {
-        case SDL_KEYUP:
-            Type = Type::Up;
-            break;
-        case SDL_KEYDOWN:
-            Type = Type::Down;
-            break;
-        default:
-            Type = Type::Unknown;
-            break;
+        case SDL_KEYUP: Type = Type::Up; break;
+        case SDL_KEYDOWN: Type = Type::Down; break;
+        default: Type = Type::Unknown;break;
     }
 
     // Parse event state.
     switch (event.state) {
-        case SDL_PRESSED:
-            State = State::Pressed;
-            break;
-        case SDL_RELEASED:
-            State = State::Released;
-            break;
-        default:
-            State = State::Unknown;
-            break;
+        case SDL_PRESSED: State = State::Pressed; break;
+        case SDL_RELEASED: State = State::Released; break;
+        default: State = State::Unknown; break;
     }
 }
 
