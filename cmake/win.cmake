1295b645c871fb8346daf61e0436f40e9da7b283
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2023-11-23 19:47:22 +0100

    Add support for compiling with MSVC.

---
+++ cmake/win.cmake
@@ -0,0 +1,41 @@
+# This file contains CMake code specific for the MSVC compiler.
+
+# List warning flags.
+set(warningList
+    /sdl
+    /W3
+    /WX)
+
+set(instrumentationList
+    /fsanitize=address)
+
+set(debugList
+    /EHsc
+    /Od
+    /RTC1
+    /Zi)
+
+set(releaseList
+    /arch:AVX2
+    /favor:INTEL64
+    /EHsc
+    /GL
+    /O2
+    /Qpar)
+
+# Setup build flags.
+if (NOT haveFlags)
+    list(JOIN warningList " " warningFlags)
+    set(CMAKE_CXX_FLAGS "${warningFlags}" CACHE STRING "" FORCE)
+endif ()
+
+if (NOT haveDebugFlags)
+    list(APPEND debugList ${instrumentationList})
+    list(JOIN debugList " " debugFlags)
+    set(CMAKE_CXX_FLAGS_DEBUG "${debugFlags}" CACHE STRING "" FORCE)
+endif ()
+
+if (NOT haveReleaseFlags)
+    list(JOIN releaseList " " releaseFlags)
+    set(CMAKE_CXX_FLAGS_RELEASE "${releaseFlags}" CACHE STRING "" FORCE)
+endif ()
